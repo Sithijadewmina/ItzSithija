1632908061f6615d842a140026121e414585af99
✦Sithija Dewmina✦ <92771213+Sithijadewmina@users.noreply.github.com>
2021-12-29 17:18:04 +0530

    Update script.js

--- script.js
+++ script.js
@@ -35,14 +35,14 @@ $(document).ready(function(){
 
     // typing text animation script
     var typed = new Typed(".typing", {
-        strings: ["Bot Developer", "Student", "A friend"],
+        strings: ["Logo Designer", "Artist" ,"Student", "A friend"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
     });
 
     var typed = new Typed(".typing-2", {
-        strings: ["Bot Developer", "Student", "A friend"],
+        strings: ["Logo Designer", "Artist" , "Student", "A friend"],
         typeSpeed: 100,
         backSpeed: 60,
         loop: true
